591878d3fb69a33f124805e326b169dbe0df4791
mbabiychuk <marichka201184@gmail.com>
2021-01-17 12:44:07 -0500

    new branch

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.views import APIView
 from rest_framework import status
 
@@ -6,20 +7,30 @@ from .serializers import ShopSerializer, ItemSerializer
 from .models import ShopModel, ItemModel
 
 
-class MyApiView(APIView):
-    def post(self, *args, **kwargs):
-        data = self.request.data
-        print(data)
-        serializer = ShopSerializer(data=data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class MyApiView(ListCreateAPIView):
+    serializer_class = ShopSerializer
+    queryset = ShopModel.objects.all()
 
-    def get(self, *args, **kwargs):
-        qs = ShopModel.objects.all()
-        serializer = ShopSerializer(qs, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class ItemApiView(ListCreateAPIView):
+    serializer_class = ItemSerializer
+    queryset = ItemModel.objects.all()
+
+
+# class MyApiView(APIView):
+#    def post(self, *args, **kwargs):
+#         data = self.request.data
+#         print(data)
+#         serializer = ShopSerializer(data=data)
+#         if not serializer.is_valid():
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#     def get(self, *args, **kwargs):
+#         qs = ShopModel.objects.all()
+#         serializer = ShopSerializer(qs, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class ReadUpdateView(APIView):
     def delete(self, *args, **kwargs):
@@ -37,22 +48,6 @@ class ReadUpdateView(APIView):
             return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
         serializer.save()
         return Response(serializer.data, status.HTTP_200_OK)
-
-
-class ItemApiView(APIView):
-    def post(self, *args, **kwargs):
-        data = self.request.data
-        print(data)
-        serializer = ItemSerializer(data=data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def get(self, *args, **kwargs):
-        qs = ItemModel.objects.all()
-        serializer = ItemSerializer(qs, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ReadUpdateItemView(APIView):
@@ -80,7 +75,7 @@ class ReadUpdateItemView(APIView):
         if id:
             qs = qs.filter(pk=id)
         if shop_id:
-            qs= qs.filter(shop_name=shop_id)
+            qs = qs.filter(shop_name=shop_id)
         serializer = ItemSerializer(qs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -94,4 +89,3 @@ class ReadShopItemView(APIView):
             qs = qs.filter(shop_name=id)
         serializer = ItemSerializer(qs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
